c2e3e4e35952f219716576f23b354a4c47d8e76f
Sergei <sergeisv113@gmail.com>
2022-02-15 16:22:08 +0200

    '13'

--- script.js
+++ script.js
@@ -178,4 +178,37 @@ console.log(person);//mom: 'Olga'
 console.log(clone);//mom = 'Ann'
 */
 //AJAX
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd')
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'current.json');//'GET', 'current.json', async, login, pass
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8;');
+    request.send();
+//readystatechange
+/*
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status ===200 ) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else  {
+            inputUsd.value = 'Error';
+        }
+*/
+  //load
+        request.addEventListener('load', () => {
+            if (request.status ===200 ) {
+                console.log(request.response);
+                const data = JSON.parse(request.response);
+                inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+            } else  {
+                inputUsd.value = 'Error';
+            }
+    })
+    //status 404,201
+    //statusText=ok, not found
+    //response
+    //readyState
 
+});
